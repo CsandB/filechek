da671199d3532833d64a3dd06ace3939956321e7
CsandB <91120423+CsandB@users.noreply.github.com>
2021-09-27 18:45:34 +0300

    Решение в несколько строк, непонятно, какие сущности в /service/
    $permissions = new \CCrmPerms(\CCrmSecurityHelper::GetCurrentUserID());
    CFile::ViewByUser(
                CFile::getbyID(int),
                [
                    "force_download" => true,
                    "cache_time"     => 86400,
                ]);

--- run_cl.php
+++ run_cl.php
@@ -1,5 +1,19 @@
 <?php /** @noinspection AutoloadingIssuesInspection */
+define('NO_KEEP_STATISTIC', 'Y');
+define('NO_AGENT_STATISTIC', 'Y');
+define('NO_AGENT_CHECK', true);
+define('DisableEventsCheck', true);
+define('NOT_CHECK_PERMISSIONS', true);
+//include_once "_prolog_before_.php";
+if (false !== strpos($_SERVER['HTTP_USER_AGENT'], "Mozilla")) {
+    require_once($_SERVER['DOCUMENT_ROOT']
+        ."/bitrix/modules/main/include/prolog_before.php");
+} else {
+    require_once "_prolog_before_.php";
+}
 
+
+use Bitrix\Main\HttpRequest;
 use Bitrix\Main\Context;
 use Bitrix\Main\Loader;
 use Bitrix\Main\LoaderException;
@@ -14,7 +28,7 @@ use Bitrix\Main\Localization\Loc;
  * @property-read integer $id    Идентификатор файла
  * @property-read string  $hash  Идентификатор объекта
  */
-abstract class RunTest
+class Run
 {
 
     /**
@@ -22,19 +36,19 @@ abstract class RunTest
      *
      * @var array $necessaryModules
      */
-    protected static $necessaryModules = [];
+    static $necessaryModules = ['main', 'crm'];
     /**
      * ID текущего файла
      *
      * @var int $fileId
      */
-    protected $fileId;
+    public $fileId;
     /**
      * Текущий файл
      *
      * @var array $file
      */
-    protected $file;
+    public $file;
     /**
      * Действие над текущим файлом
      * get url file
@@ -42,143 +56,31 @@ abstract class RunTest
      *
      * @var string $action
      */
-    protected $action;
+    public $action;
+    public $hash;
     /**
      * Ошибки при выполнении
      *
      * @var array $errors
      */
-    protected $errors = [];
+    public $errors = [];
+    /**
+     * @var HttpRequest $request
+     */
+    protected $request = array();
+
+    private $debug = true;
 
     function __construct()
     {
-        $this->fileId = $this->getFileId();
-        $this->file = '';
-    }
-
-    /**
-     * @return int
-     */
-    public function getFileId(): int
-    {
-        $request = Context::getCurrent()->getRequest();
-
-        return $request->getPost('id');
-    }
-
-    /**
-     * @return array
-     */
-    public function getFile()
-    {
-        $arFile['file'] = CFile::GetFileArray($this->fileId);
-        $arFile['TO_PRINT'] = CHTTP::URN2URI($arFile["SRC"]);
-        $arFile['TO_GET'] = '';
-
-        return $arFile;
-    }
-
-    // Выводим ошибки
-    // todo ::showError($err) ['ERRORS'] = $this->errors;
-
-    /**
-     * Обрабатываем действие
-     */
-    protected function prepareAction()
-    {
-        switch ($this->action) {
-            case 'print':
-                break;
-            case 'get':
-                $this->actionGet();
-                break;
-            default:
-                break;
-        }
-    }
-
-    /**
-     * Действие Скачать
-     */
-    protected function actionGet()
-    {
-        $request = Context::getCurrent()->getRequest();
-        $cookie = $request->getCookieRawList();
-
-        /* todo
-          [BITRIX_SM_GUEST_ID] => 9852
-          [BITRIX_SM_TIME_ZONE] => -180
-            [BITRIX_SM_UIDH] => f3cad70823fa3209eca8d33b64660f3
-            [BITRIX_SM_UIDL] => admin
-            [BITRIX_SM_SALE_UID] => 3
-            [BITRIX_SM_LOGIN] => admin
-            [BITRIX_SM_SOUND_LOGIN_PLAYED] => Y
-            [PHPSESSID] => 81e7386234b8b7f9de8d5690edf46c
-          */
-
-        return true;
-    }
-
-    /**
-     * Действие Ссылка для скачивания
-     */
-    protected function actionPrint()
-    {
-        $request = Context::getCurrent()->getRequest();
-        if ($this->action === 'print') {
-
-            $initiator = static::currentUserId();
-
-            //работа с сущностями
-            $status = static::getOne(['ID' => $initiator]);
-
-            $this->getData($initiator, $status);
-
-            // Генерируем файл
-            $this->makeFile();
-        }
-
-        return true;
-    }
-
-    /**
-     * Идентификатор текущего пользователя
-     *
-     * @return int
-     */
-    public static function currentUserId(): ?int
-    {
-        global $USER;
-        if (is_object($USER) && $USER->IsAuthorized()) {
-            return $USER->GetID();
-        }
-
-        return null;
-    }
-
-    public static function getOne($param, $additionalParams = [])
-    {
-        if ( ! $param) {
-            return null;
-        }
+        $this->request = Context::getCurrent()->getRequest();
+        //$this->action='';
 
         static::includeNecessaryModules();
+        $this->prepareRequestData();
 
-        if (isArrayAssoc($param)) {
-            $params = [
-                'filter' => $param,
-            ];
-            if (array_key_exists('select', $additionalParams)) {
-                $params['select'] = $additionalParams['select'];
-            }
+        $this->prepareAction();
 
-            $res = static::getList($params, $additionalParams);
-            $arRes = $res ? $res->Fetch() : null;
-
-        }
-
-
-        return null;
     }
 
     /**
@@ -186,7 +88,7 @@ abstract class RunTest
      *
      * @return boolean
      */
-    protected static function includeNecessaryModules(): bool
+    static function includeNecessaryModules(): bool
     {
         $result = true;
         foreach (static::$necessaryModules as $moduleName) {
@@ -222,6 +124,155 @@ abstract class RunTest
         }
     }
 
+    protected function prepareRequestData()
+    {
+        $this->fileId = $this->getFileId();
+        $this->file = $this->getFile();
+        $this->action = $this->request->get('action') !== null
+            ?
+            $this->request->get('action')
+            :
+            'print';
+    }
+
+    // Выводим ошибки
+    // todo ::showError($err) ['ERRORS'] = $this->errors;
+
+    /**
+     * @return int
+     */
+    public function getFileId(): int
+    {
+        if ($this->debug) {
+            return 104;
+        }
+
+        return $this->request->get('id');
+
+    }
+
+    /**
+     * @return array
+     */
+    public function getFile()
+    {
+        $arFile['file'] = CFile::GetFileArray($this->fileId);
+        $arFile['TO_PRINT'] = CHTTP::URN2URI($arFile['file']["SRC"]);
+        $arFile['SRC'] = $arFile['file']["SRC"];
+        $arFile['PATH'] = $_SERVER['DOCUMENT_ROOT'].$arFile['file']["SRC"];
+
+        return $arFile;
+    }
+
+    /**
+     * Обрабатываем действие
+     */
+    function prepareAction()
+    {
+        switch ($this->action) {
+            case 'print':
+                break;
+            case 'get':
+                $this->actionGet();
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * Действие Скачать
+     */
+    function actionGet()
+    {
+        static::getOne();
+        CFile::ViewByUser(
+            $this->file['file']['file'],
+            [
+                "force_download" => true,
+                "cache_time"     => 86400,
+            ]);
+
+        exit;
+    }
+
+    public static function getOne($param = [], $additionalParams = [])
+    {
+        if (empty($param)) {
+
+            $currentUserId = \CCrmSecurityHelper::GetCurrentUserID()
+                ?
+                \CCrmSecurityHelper::GetCurrentUserID()
+                :
+                static::currentUserId();
+
+            $userPermissions
+                = new \CCrmPerms($currentUserId);
+
+            return $userPermissions;
+        }
+
+
+        if (isArrayAssoc($param)) {
+            $params = [
+                'filter' => $param,
+            ];
+            if (array_key_exists('select', $additionalParams)) {
+                $params['select'] = $additionalParams['select'];
+            }
+
+            $res = static::getList($params, $additionalParams);
+            $arRes = $res ? $res->Fetch() : null;
+
+        }
+
+
+        return null;
+    }
+
+    /**
+     * Идентификатор текущего пользователя
+     *
+     * @return int
+     * @todo
+     */
+    public static function currentUserId(): ?int
+    {
+        global $USER;
+        if (isset($USER)
+            && ((get_class($USER) === 'CUser')
+                || ($USER instanceof CUser))
+        ) {
+            if (is_object($USER) && $USER->IsAuthorized()) {
+                return $USER->GetID();
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Действие Ссылка для скачивания
+     */
+    function actionPrint()
+    {
+        $request = Context::getCurrent()->getRequest();
+        if ($this->action === 'print') {
+
+            $initiator = static::currentUserId();
+
+            //работа с сущностями
+            $status = static::getOne(['ID' => $initiator]);
+
+            $this->getData($initiator, $status);
+
+            // Генерируем файл
+            $this->makeFile();
+        }
+
+        return true;
+    }
+
 
     /**
      * Получить результат запроса списка согласно указанным параметрам
@@ -231,42 +282,30 @@ abstract class RunTest
      *
      * @return array|bool|integer|CDBResult|CIBlockResult
      */
-    abstract public static function getList(
+    /*public function getList(
         $params = [],
         $additionalParams = []
-    );
+    )*/
 
     /**
      * Получаем данные о пользователе
      * и возможности скачать файл $this->fileId
      */
-    protected function getData($initiator, $status)
+    function getData($initiator, $status)
     {
 
         return true;
     }
 
-
-    protected function makeFile()
+    function makeFile()
     {
-        /** @var $GLOBALS ['APPLICATION'] \CAllMain */
-        $GLOBALS['APPLICATION']->RestartBuffer();
-
-        $file = '/path/to/file/outside/www/secret.pdf';
-
-        header('Content-Description: File Transfer');
-        header('Content-Type: application/octet-stream');
-        header('Content-Disposition: attachment; filename='.basename($file));
-        header('Content-Transfer-Encoding: binary');
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-        header('Pragma: public');
-        header('Content-Length: '.filesize($file));
-        ob_clean();
-        flush();
-        readfile($file);
-        exit;
+        return true;
     }
 
 
 }
+
+
+$retc = new Run();
+
+$ret = $retc->makeFile();
